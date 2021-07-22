e6e6241a0a1865fdb0a2a0477afe774235d4d1bb
dreyliky <dreyliky@gmail.com>
2021-07-22 17:48:43 +0300

    (feature) add telegram bot listener

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TelegramBotListenerService } from '@Features/telegram-bot';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [],
+	controllers: [
+		AppController
+	],
+	providers: [
+		AppService,
+		TelegramBotListenerService
+	]
 })
-export class AppModule {}
+export class AppModule {
+	constructor(
+		// Create instances of services
+		public readonly telegramBotListenerService: TelegramBotListenerService
+	) {}
+}
--- src/features/telegram-bot/services/listener.service.ts
+++ src/features/telegram-bot/services/listener.service.ts
@@ -1,7 +1,20 @@
 import { Injectable } from "@nestjs/common";
-import {  } from 'node-telegram-bot-api';
+import * as TelegramBot from 'node-telegram-bot-api';
 
 @Injectable()
 export class TelegramBotListenerService {
+    private readonly botToken = '1939938120:AAFhFp1fTUdKtF5b4xGwHh6Yvw3zv2MkJOA';
+    private readonly botInstance: TelegramBot;
 
+    constructor() {
+        this.botInstance = new TelegramBot(this.botToken, { polling: true });
+
+        this.initUserMessageObserver();
+    }
+
+    private initUserMessageObserver(): void {
+        this.botInstance.on('message', (message) => {
+            console.log(message);
+        });
+    }
 }
